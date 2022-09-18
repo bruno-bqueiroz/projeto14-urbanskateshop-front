e80435c736b920f78cd7ec271b83298c0415f590
Math <math@pop-os.localdomain>
2022-09-18 00:25:37 -0300

    arrumando os gits

--- src/componentes/App.js
+++ src/componentes/App.js
@@ -1,19 +1,27 @@
 import GlobalStyle from "../style/globalStyles";
-import UserContext from '../context/UserContext'; 
+import UserContext from './context/UserContext'; 
 import { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import Home from "./Home";
-import Login from "./Login";
-import Cadastro from "./Cadastro";
-import Carrinho from "./Carrinho";
+import Home from "./Home/Home";
+import Login from "./signIn-signUp/SignIn";
+import Cadastro from "./signIn-signUp/SignUp";
+import Carrinho from "./Carrinho/Carrinho";
+
+import "../style/style.css"
 
 export default function App() {
   const [token, setToken] = useState([]);
+  const [user, setUser] = useState({})
+  
+  
   return (
     <>
     <GlobalStyle/>
-    <UserContext.Provider value={{token, setToken}}>
+    <UserContext.Provider value={{
+      token, setToken,
+      user, setUser
+      }}>
       <BrowserRouter>
         <Routes>
           <Route path='/' element = {<Home />}/>
