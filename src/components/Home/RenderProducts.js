3740345b8c7b4f3e3adda5d1844aa03c8d051ee3
Math <math@pop-os.localdomain>
2022-11-28 00:28:13 -0300

    fixed page checkout

--- src/components/Home/RenderProducts.js
+++ src/components/Home/RenderProducts.js
@@ -1,20 +1,30 @@
-import { Link } from "react-router-dom"
 import styled from "styled-components" 
+import { useNavigate } from "react-router-dom"
+import { useContext } from "react";
+
+import UserContext from "../shared/context/UserContext";
 
 export default function RenderizarHome({
     product
 }){
+    const navigate = useNavigate();
+    const {carouselInterval} = useContext(UserContext);
+    console.log(carouselInterval)
+
+    function clearintervalAndEnterProduct(valueId){
+        clearInterval(carouselInterval)
+        navigate(`/${valueId}`)
+    }
+
     return (
         <>
             {product.map((value, index) => 
-                <Caixa1 key={index}>
-                    <Link to={`/${value._id}`}>
+                <Caixa1 key={index} onClick={()=>clearintervalAndEnterProduct(value._id)}>
                     <img src={value.url_image} alt = {value.title} />
                     <div>
                         <h1>{value.title}</h1>
                         <p>R$ {(value.newValue/100).toFixed(2)}</p>            
                     </div>
-                    </Link>
                 </Caixa1>
             )} 
         </>
